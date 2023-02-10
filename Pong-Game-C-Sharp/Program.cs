f6f5f6ba0fd75f7768ade6123420f1df6cf00b65
wasiu <wasiu@DESKTOP-CQ1MLRN>
2023-02-10 22:16:22 +0600

    Add project files.

---
+++ Pong-Game-C-Sharp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fieldLength = 50, fieldWidth = 15;
+            const char fieldTile = '#';
+            string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLength));
+
+            const int racketLength = fieldWidth / 4;
+            const char racketTile = '|';
+
+            int leftRacketHeight = 0;
+            int rightRacketHeight = 0;
+
+         
+
+            while(true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(line);
+
+                Console.SetCursorPosition(0, fieldWidth);
+                Console.WriteLine(line);
+
+                for (int i = 0;  i < racketLength; i++)
+                {
+                    Console.SetCursorPosition(0, i + 1 + leftRacketHeight);
+                    Console.WriteLine(racketTile);
+                    Console.SetCursorPosition(fieldLength-1, i + 1 + rightRacketHeight);
+                    Console.WriteLine(racketTile);
+                }
+
+                while (!Console.KeyAvailable)
+                {
+
+                }
+
+                switch(Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (rightRacketHeight > 0)
+                        {
+                            rightRacketHeight--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (rightRacketHeight < fieldLength - racketLength - 1)
+                        {
+                            rightRacketHeight++;
+                        }
+                        break;
+                    case ConsoleKey.W:
+                        if (rightRacketHeight > 0)
+                        {
+                            leftRacketHeight--;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                        if (rightRacketHeight < fieldLength - racketLength - 1)
+                        {
+                            leftRacketHeight++;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
